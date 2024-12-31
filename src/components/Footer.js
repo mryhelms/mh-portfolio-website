63d8039ebaaa80f76fbffda5af6adb56a1bb1d5d
mryhelms <mryanhelms@gmail.com>
2024-12-31 10:04:57 -0800

    Configuring for website

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,9 @@ function Footer() {
         <footer className="footer">
             <p>© {new Date().getFullYear()} Matt Helms. All rights reserved.</p>
             <p>
-                <a href="mailto:youremail@example.com">Email</a> |
+                <a href="mailto:mryanhelms@gmail.com">Email</a> |
                 <a href="https://github.com/yourusername"> GitHub</a> |
-                <a href="https://linkedin.com/in/yourusername"> LinkedIn</a>
+                <a href="https://linkedin.com/in/matthew-helms-409b28153"> LinkedIn</a>
             </p>
         </footer>
     );
